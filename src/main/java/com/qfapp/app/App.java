324e09ccb3c30d235fba174aaedc9dbdfb79ba0f
root <root@ip-172-31-4-85.ap-south-1.compute.internal>
2022-09-21 01:50:05 +0000

    updated

---
+++ src/main/java/com/qfapp/app/App.java
@@ -0,0 +1,13 @@
+package com.qfapp.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
